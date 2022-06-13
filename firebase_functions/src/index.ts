a48c72e564ef4194513cec619c36202e95b5af4d
xanderjakeq <xanderjakeq@gmail.com>
2022-06-13 16:11:09 -0400

    bugfix: prevent on_update loop

--- firebase_functions/src/index.ts
+++ firebase_functions/src/index.ts
@@ -3,6 +3,16 @@ import { firestore } from "firebase-admin";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
+//
+type Farm = {
+    name: string;
+    displayName: string;
+    phone?: string;
+    openHours?: string;
+    image?: string;
+    created_at?: firestore.Timestamp;
+    updated_at?: firestore.Timestamp;
+};
 
 exports.on_create_farm = functions.firestore
     .document("farms/{name}")
@@ -15,6 +25,26 @@ exports.on_create_farm = functions.firestore
 exports.on_update_farm = functions.firestore
     .document("farms/{name}")
     .onUpdate((snap, context) => {
+        const { name, displayName, phone, openHours } =
+            snap.before.data() as Farm;
+
+        const {
+            name: _n,
+            displayName: _d,
+            phone: _p,
+            openHours: _o,
+        } = snap.after.data() as Farm;
+
+        if (
+            name === _n &&
+            displayName === _d &&
+            phone === _p &&
+            phone === _p &&
+            openHours === _o
+        ) {
+            return;
+        }
+
         snap.after.ref.update({
             updated_at: firestore.Timestamp.fromDate(new Date()),
         });
